e3e98e6aeff2dda6dca1ecb48cdc36b4b3407efc
AleksaMilosavljevic <aleksa.milosavljevic1998@gmail.com>
2022-04-15 00:13:30 +0200

    API autentifikovan preko korisnika

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ProizvodjacController;
 use App\Http\Controllers\VlasnikController;
 use Illuminate\Http\Request;
@@ -18,14 +19,17 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('proizvodjac', [ProizvodjacController::class, 'index']);
 Route::get('proizvodjac/{proizvodjac}', [ProizvodjacController::class, 'show']);
-Route::delete('proizvodjac/{proizvodjac}', [ProizvodjacController::class, 'destroy']);
 
 Route::get('vlasnik', [VlasnikController::class, 'index']);
 Route::get('vlasnik/{vlasnik}', [VlasnikController::class, 'show']);
-Route::post('vlasnik', [VlasnikController::class, 'store']);
-Route::delete('vlasnik/{vlasnik}', [VlasnikController::class, 'destroy']);
 
+Route::post('register', [AuthController::class, 'register']);
+Route::post('login', [AuthController::class, 'login']);
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(['middleware' => ['auth:sanctum']], function () {
+
+    Route::delete('proizvodjac/{proizvodjac}', [ProizvodjacController::class, 'destroy']);
+    Route::post('vlasnik', [VlasnikController::class, 'store']);
+    Route::delete('vlasnik/{vlasnik}', [VlasnikController::class, 'destroy']);
+    Route::post('logout', [AuthController::class, 'logout']);
 });
